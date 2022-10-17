aa67e29a72dc59dd9243179139a333058c5072d2
tmessous <34687736+tmessous@users.noreply.github.com>
2022-10-17 18:44:52 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -117,7 +117,7 @@ services:
         - 50000:50000
     container_name: jenkins
     volumes:
-        - /home/debian/jenkins:/var/jenkins_home
+        - ${HOME}/jenkins:/var/jenkins_home
         - /var/run/docker.sock:/var/run/docker.sock
         - /usr/local/bin/docker:/usr/local/bin/docker
 EOF
